8b0b8d5d7b72f281aaeb21274f978477259e0e60
Ssuamje <side2850@naver.com>
2023-12-06 21:58:02 +0900

    SPRING LV 2 clear

--- v1/SPRING_LV_2_김영한_이동진_그리고/src/main/java/com/example/dongglee/domain/Movie.java
+++ v1/SPRING_LV_2_김영한_이동진_그리고/src/main/java/com/example/dongglee/domain/Movie.java
@@ -1,15 +1,31 @@
 package com.example.dongglee.domain;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.time.LocalDateTime;
 
-@Builder
 @Getter
+@Entity
+@NoArgsConstructor
 public class Movie {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(nullable = false)
 	private String director;
+	@Column(nullable = false)
 	private String title;
+	@Column(nullable = false)
 	private LocalDateTime filmedAt;
+
+	@Builder
+	public Movie(String director, String title, LocalDateTime filmedAt) {
+		this.director = director;
+		this.title = title;
+		this.filmedAt = filmedAt;
+	}
 }
