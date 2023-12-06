80fb7e36bde428d359a03860d885d4e2662d1ae3
Ssuamje <side2850@naver.com>
2023-12-06 21:31:52 +0900

    assertions 수정

--- v1/JAVA_LV_2_답답하면_니들이_뛰던가/src/main/java/com/example/lent/domain/User.java
+++ v1/JAVA_LV_2_답답하면_니들이_뛰던가/src/main/java/com/example/lent/domain/User.java
@@ -1,19 +1,14 @@
 package com.example.lent.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /*Entity가 뭘까요?*/
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class User {
 	private Long userId;
 	private String name;
 	private boolean isBanned;
-
-	public User(Long userId, String name, boolean isBanned) {
-		this.userId = userId;
-		this.name = name;
-		this.isBanned = isBanned;
-	}
 }
