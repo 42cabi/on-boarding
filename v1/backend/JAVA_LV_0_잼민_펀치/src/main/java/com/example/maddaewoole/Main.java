d75e1a03ca9a2c56f46b05f5608ad1422810d614
chyo1 <hyowon3856@gmail.com>
2024-01-05 17:49:33 +0900

    hyowchoi2

--- v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Main.java
+++ v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Main.java
@@ -7,14 +7,15 @@ public class Main {
 		Daewoole daewook = new Daewoole(jiwon.getStr());
 
 		int attack_cnt = 0;
-		while (true){
+		while (true) {
 			String attack = jiwon.prov();
 			int daewookAnger = daewook.beProvinced(attack);
 			System.out.println("지원은 " + attack + "를 시전하여 대욱의 분노를 " + daewookAnger + " 증가시켰다.");
 			daewook.now_anger += daewookAnger;
 			System.out.println("현재 대욱의 분노 수치 : " + daewook.now_anger);
-			if (daewook.now_anger > daewook.getAnger_threshold())
+			if (daewook.now_anger > daewook.getAnger_threshold()) {
 				break;
+			}
 			attack_cnt++;
 		}
 		System.out.println("대욱을 도발한 횟수 : " + attack_cnt + "회");
