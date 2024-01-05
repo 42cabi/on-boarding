36761c13db91043d12555c39ddd80175a7fda6a2
saewoo1 <wooozooo1123@icloud.com>
2024-01-05 16:45:36 +0900

    FEAT: 현재 분노 수치가 한계를 넘었는지 검수하는 기능

--- v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Daewoole.java
+++ v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Daewoole.java
@@ -1,5 +1,38 @@
 package com.example.maddaewoole;
 
-public class Daewoole {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
+public class Daewoole {
+    private static final int MIN_LIMIT = 80;
+    private static final int MAX_LIMIT = 120;
+    private int anger;
+    private final int limit;
+    private Map<String, Integer> provokeMap = new HashMap<>();
+
+    public Daewoole(List<String> mentions) {
+        anger = 0;
+        limit = generateRandomNumberInRange(MIN_LIMIT, MAX_LIMIT);
+        provokeMap = learnMentions(mentions);
+    }
+
+    private Map<String, Integer> learnMentions(List<String> mentions) {
+        return null;
+    }
+
+    // 범위 내의 랜덤값 생성
+    private int generateRandomNumberInRange(int min, int max) {
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
+    }
+
+    public boolean isOverLimit() {
+        return anger >= limit;
+    }
+
+    public int getAnger() {
+        return anger;
+    }
 }
