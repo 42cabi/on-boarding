80800cd740304cf237389e59e5d52c37e108bc66
saewoo1 <wooozooo1123@icloud.com>
2024-01-05 16:33:02 +0900

    FEAT: 랜덤한 도발 문자열을반환하는 기능

--- v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Jpark2.java
+++ v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Jpark2.java
@@ -1,5 +1,20 @@
 package com.example.maddaewoole;
 
-public class Jpark2 {
+import java.util.List;
+import java.util.Random;
 
+public class Jpark2 {
+    private final List<String> mentions = List.of("당신의 지각비, 회식비로 대체되었다", "코딩 그렇게 하는거 아닌데", "오늘 저녁은 감탄계");
+
+
+    public String provoke() {
+        return mentions.get(generateRandomNumberByIndex());
+    }
+
+    private int generateRandomNumberByIndex() {
+        int range = mentions.size();
+
+        Random random = new Random();
+        return random.nextInt(range);
+    }
 }
