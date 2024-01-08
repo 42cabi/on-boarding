edfaf0fd8e05f0160df2cb9a250addf9f0e1d4d8
saewoo1 <wooozooo1123@icloud.com>
2024-01-08 18:32:45 +0900

    FEAT: 구현 클래스 메서드 작성

--- v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/BinarySearch.java
+++ v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/BinarySearch.java
@@ -2,4 +2,13 @@ package com.example.algorithmbot;
 
 public class BinarySearch implements Algorithm {
 
+    @Override
+    public boolean isSolution(Algorithm algorithm) {
+        return algorithm instanceof BinarySearch;
+    }
+
+    @Override
+    public String getSolution() {
+        return "반띵 ㄱㄱ";
+    }
 }
--- v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/DFS.java
+++ v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/DFS.java
@@ -2,4 +2,13 @@ package com.example.algorithmbot;
 
 public class DFS implements Algorithm {
 
+    @Override
+    public boolean isSolution(Algorithm algorithm) {
+        return algorithm instanceof DFS;
+    }
+
+    @Override
+    public String getSolution() {
+        return "나는 한 놈씩 조져";
+    }
 }
