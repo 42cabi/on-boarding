482c26e6c5856b884c1cfac55530dba5f77d48dc
saewoo1 <wooozooo1123@icloud.com>
2024-01-08 21:26:01 +0900

    REFACTOR: Controller 주석 추가

--- v1/backend/SPRING_LV_1_콩_심은_데_콩_난다/src/main/java/com/example/kong/KongApplication.java
+++ v1/backend/SPRING_LV_1_콩_심은_데_콩_난다/src/main/java/com/example/kong/KongApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 님이 있는 패키지 기준으로 컴포넌트 스캔을 시작합니다..
 @SpringBootApplication
 public class KongApplication {
 
