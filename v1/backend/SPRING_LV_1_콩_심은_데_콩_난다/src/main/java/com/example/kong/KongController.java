f7a69aecf3670ac68b2c05491ea4b1820741702d
saewoo1 <wooozooo1123@icloud.com>
2024-01-08 20:56:22 +0900

    FEAT: 과제 해결

--- v1/backend/SPRING_LV_1_콩_심은_데_콩_난다/src/main/java/com/example/kong/KongController.java
+++ v1/backend/SPRING_LV_1_콩_심은_데_콩_난다/src/main/java/com/example/kong/KongController.java
@@ -1,11 +1,16 @@
 package com.example.kong;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /* Spring Boot에서 컨트롤러는 어떻게 인식할까요? */
+@RestController
+@RequiredArgsConstructor
 public class KongController {
 
-	private KongService kongService;
+	private final KongService kongService;
 
 	@GetMapping("/reaction")
 	public String reaction() {
