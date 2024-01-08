40cb46d718a2382fb2515a2706d3381898e8d45a
saewoo1 <wooozooo1123@icloud.com>
2024-01-08 20:13:47 +0900

    FEAT: solveAlgorithm 메서드 작성

--- v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/Wchae.java
+++ v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/Wchae.java
@@ -1,5 +1,32 @@
 package com.example.algorithmbot;
 
-public class Wchae {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
 
+public class Wchae {
+    private final Map<Algorithm, Integer> algorithmMap = new HashMap<>();
+    private final Random random = new Random();
+
+    public String solveAlgorithm(Algorithm algorithm) {
+        int solveCount = algorithmMap.getOrDefault(algorithm, 0);
+
+        Optional<Algorithm> algo = algorithmMap.keySet()
+                .stream()
+                .filter(elem -> elem.isSolution(algorithm))
+                .findFirst();
+        if (algo.isPresent()) {
+            double solveProbability = solveCount * 0.1;
+            double percentage = random.nextDouble();
+
+            if (percentage < solveProbability) {
+                algorithmMap.put(algorithm, solveCount + 1);
+                return algorithm.getSolution();
+            }
+            return "스트레스 받네...";
+        }
+        algorithmMap.put(algorithm, solveCount + 1);
+        return "스트레스 받네...";
+    }
 }
