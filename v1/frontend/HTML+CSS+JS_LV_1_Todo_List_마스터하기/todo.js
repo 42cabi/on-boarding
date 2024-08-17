ccb4c087fe6914cb4535af2c5cbe7754fe221e1d
Jihyun Kim <ksk208105@gmail.com>
2024-08-17 19:21:17 +0900

    [FE] FEAT: Todo List 마스터하기(bonus)

--- v1/frontend/HTML+CSS+JS_LV_1_Todo_List_마스터하기/todo.js
+++ v1/frontend/HTML+CSS+JS_LV_1_Todo_List_마스터하기/todo.js
@@ -1,58 +1,116 @@
+/* values */
 const inputText = document.getElementById("input-text");
-const inputButton = document.getElementById("add-todo");
+const inputBtn = document.getElementById("add-todo");
 const todoList = document.getElementById("todo-list");
 
-inputText.addEventListener("keydown", handleInputText);
-inputButton.addEventListener("click", handleInputText);
+const showAllBtn = document.getElementById("show-all");
+const showTodoBtn = document.getElementById("show-todo");
+const showDoneBtn = document.getElementById("show-done");
 
+const TODO_OBJ_KEY = "todoObj";
+let todoObjs = [];
+
+
+/* render from localstorage */
+const saveTodoObjs = localStorage.getItem(TODO_OBJ_KEY);
+if (saveTodoObjs) {
+  let parsedObjs = JSON.parse(saveTodoObjs);
+  todoObjs = parsedObjs;
+  parsedObjs.forEach(createNewTodo);
+}
+
+/* event listener for buttons */
+inputText.addEventListener("keydown", handleInputText);
+inputBtn.addEventListener("click", handleInputText);
+showAllBtn.addEventListener("click", handleShowAll);
+showTodoBtn.addEventListener("click", handleShowTodo);
+showDoneBtn.addEventListener("click", handleShowDone);
+
+
+/** function */
+/* save to localstorage */
+function saveToLocal() {
+  localStorage.setItem(TODO_OBJ_KEY, JSON.stringify(todoObjs));
+}
+
+/* create todo list for html: li ( button, button, span ) */
+function createNewTodo(newTodo) {
+  const newList = document.createElement("li");
+  newList.id = newTodo.id;
+  newList.className = "flex-row";
+
+  const todoText = document.createElement("span");
+  todoText.innerText = newTodo.text;
+
+  const doneBtn = document.createElement("button");
+  if (newTodo.completed) {
+    todoText.classList.toggle("completed");
+    doneBtn.className = "done-btn";
+  } else {
+    doneBtn.className = "todo-btn";
+  }
+  doneBtn.addEventListener("click", () => {
+    todoText.classList.toggle("completed");
+    if (todoText.classList.contains("completed")) {
+      doneBtn.className = "done-btn";
+      newTodo.completed = true;
+    } else {
+      doneBtn.className = "todo-btn";
+      newTodo.completed = false;
+    }
+    saveToLocal();
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.className = "trash-btn";
+  deleteBtn. addEventListener("click", () => {
+    // newList.parentNode.removeChild(newList);
+    newList.remove();
+    todoObjs = todoObjs.filter((todo) => todo.id !== parseInt(newList.id))
+    saveToLocal();
+  });
+
+  newList.appendChild(doneBtn);
+  newList.appendChild(deleteBtn);
+  newList.appendChild(todoText);
+  todoList.appendChild(newList);
+}
+
+/* handling for events */
 function handleInputText(e) {
   if (e.isComposing) return;
   if (e.key === "Enter" || e.type === "click") {
     let inputValue = inputText.value;
-    console.log(inputValue);
+    inputText.value = "";
+
     if (!inputValue) {
       alert("It's empty!");
       return;
     }
 
-    const li = document.createElement("li");
-    li.className = "flex-row";
+    const newTodo = {
+      id: Date.now(),
+      text: inputValue,
+      completed: false
+    };
 
-    const doneBtn = document.createElement("button");
-    doneBtn.className = "todo-btn";
-    doneBtn.addEventListener("click", () => {
-      span.classList.toggle("completed");
-      if (span.classList.contains("completed")) {
-        doneBtn.className = "done-btn";
-      } else {
-        doneBtn.className = "todo-btn";
-      }
-    });
-
-    const removeBtn = document.createElement("button");
-    removeBtn.className = "trash-btn";
-    removeBtn.addEventListener("click", () => {
-      li.parentNode.removeChild(li);
-    });
-
-    const span = document.createElement("span");
-    span.innerText = inputValue;
-
-    li.appendChild(doneBtn);
-    li.appendChild(removeBtn);
-    li.appendChild(span);
-    todoList.appendChild(li);
-    inputText.value = "";
+    todoObjs.push(newTodo);
+    createNewTodo(newTodo);
+    saveToLocal();
   }
 }
 
-// 전체: [1, 2, 3, 4, 5]
+function handleShowAll() {
+  todoList.innerHTML = "";
+  todoObjs.forEach(createNewTodo);
+}
 
-// 진행중: [1, 3, 4] => [0, 1, 2]
+function handleShowTodo() {
+  todoList.innerHTML = "";
+  todoObjs.filter(todo => !todo.completed).forEach(createNewTodo);
+}
 
-// 완료: [ 2, 5] => [1, 2]
-
-
-// let allLocal = [];
-// let todoLocal = [];
-// let doneLocal = [];
+function handleShowDone() {
+  todoList.innerHTML = "";
+  todoObjs.filter(todo => todo.completed).forEach(createNewTodo);
+}
