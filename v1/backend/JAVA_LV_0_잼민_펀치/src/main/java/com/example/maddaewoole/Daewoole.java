d611f2333c79f11819c98f817ea3d094fdfc8821
lamodadite <nmb11921@naver.com>
2024-12-26 20:36:23 +0900

    [BE] FEAT: java_lv0 기능 구현

--- v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Daewoole.java
+++ v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Daewoole.java
@@ -1,5 +1,42 @@
 package com.example.maddaewoole;
 
+import static com.example.maddaewoole.Jpark2.MENT1;
+import static com.example.maddaewoole.Jpark2.MENT2;
+import static com.example.maddaewoole.Jpark2.MENT3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class Daewoole {
 
+	private int angerLevel;
+	private int angerLimit;
+	private Map<String, Integer> angerMap;
+
+	public Daewoole() {
+		angerLevel = 0;
+		angerLimit = new Random().nextInt(41) + 80;
+		angerMap = new HashMap<>();
+		angerMap.put(MENT1, new Random().nextInt(21));
+		angerMap.put(MENT2, new Random().nextInt(21) + 10);
+		angerMap.put(MENT3, new Random().nextInt(21) + 30);
+	}
+
+	public int provoked(String ment) {
+		if (ment.equals(MENT1) || ment.equals(MENT2) || ment.equals(MENT3)) {
+			angerLevel += angerMap.get(ment);
+			return angerMap.get(ment);
+		}
+		System.out.println(ment + " 이라는 도발 멘트는 없습니다");
+		return 0;
+	}
+
+	public boolean isOverAngerLimit() {
+		return this.angerLevel >= this.angerLimit;
+	}
+
+	public int getAngerLevel() {
+		return angerLevel;
+	}
 }
--- v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Jpark2.java
+++ v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Jpark2.java
@@ -1,5 +1,25 @@
 package com.example.maddaewoole;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Jpark2 {
 
+	static final String MENT1 = "당신의 지각비, 회식비로 대체되었다";
+	static final String MENT2 = "코딩 그렇게 하는거 아닌데";
+	static final String MENT3 = "오늘 저녁은 감탄계";
+
+	private final List<String> provocations = new ArrayList<>();
+
+	public Jpark2() {
+		provocations.add(MENT1);
+		provocations.add(MENT2);
+		provocations.add(MENT3);
+	}
+
+	public String provocate() {
+		int idx = new Random().nextInt(provocations.size());
+		return provocations.get(idx);
+	}
 }
