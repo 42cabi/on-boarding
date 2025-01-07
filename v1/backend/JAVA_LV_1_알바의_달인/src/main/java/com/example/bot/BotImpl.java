9bdaa53ec555a43be276ee51c0f753af6a74022c
duckgii <shinyonggi@naver.com>
2025-01-07 18:09:50 +0900

    FEAT : 각 주문별 적절한 메서드 호출 기능

--- v1/backend/JAVA_LV_1_알바의_달인/src/main/java/com/example/bot/BotImpl.java
+++ v1/backend/JAVA_LV_1_알바의_달인/src/main/java/com/example/bot/BotImpl.java
@@ -1,14 +1,49 @@
 package com.example.bot;
 
+import java.util.HashMap;
+
 public class BotImpl implements JiwonBehavior {
+
+	private final HashMap<String, Integer> menu;
+
+	private BotImpl() {
+		menu = new HashMap<String, Integer>();
+	}
 
 	@Override
 	public void takeOrder(String order) {
-
+		switch (Parse.caseCheck(order)) {
+			case ADD_ONE:
+				this.addOne(Parse.split(order));
+				break;
+			case ADD_TWO:
+				this.addTwo(Parse.split(order));
+				break;
+			case DELETE:
+				this.delete(Parse.split(order));
+				break;
+			case FiND:
+				this.find(Parse.split(order));
+				break;
+			case COUNT:
+				this.count(Parse.split(order));
+				break;
+			case END:
+				this.result();
+				break;
+		}
 	}
 
 	@Override
 	public void result() {
 
 	}
+
+	private enum ReqType {
+		ADD_ONE, ADD_TWO, DELETE, FIND, COUNT, END
+	}
+
 }
+
+//parse 클래스를 만든다
+//
