2bd0692b5eb2c34beb20c6074fd2a9259f61c9b9
lamodadite <nmb11921@naver.com>
2025-01-07 18:12:54 +0900

    FEAT : Main 클래스 구현

--- v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/Main.java
+++ v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/Main.java
@@ -1,8 +1,20 @@
 package com.example.algorithmbot;
 
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Wchae wchae = new Wchae();
+		List<Algorithm> algorithmList = List.of(new BFS(), new DFS(), new TwoPointer(),
+				new BinarySearch(), new DP());
 
+		for (int i = 0; i < 20; i++) {
+			Algorithm first = algorithmList.get((int) (Math.random() * 5));
+			Algorithm second = algorithmList.get((int) (Math.random() * 5));
+			System.out.println(wchae.solveAlgorithm(first));
+			System.out.println(wchae.solveAlgorithm(second));
+			System.out.println();
+		}
 	}
 }
