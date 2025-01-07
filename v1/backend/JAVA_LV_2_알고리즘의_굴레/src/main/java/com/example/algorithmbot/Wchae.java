725350f74b91e6e830fcd26780f90fa922a063c8
lamodadite <nmb11921@naver.com>
2025-01-07 18:03:16 +0900

    FEAT : Wchae 클래스 구현

--- v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/Wchae.java
+++ v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/Wchae.java
@@ -1,5 +1,34 @@
 package com.example.algorithmbot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Wchae {
 
+	private final Map<Algorithm, Integer> algorithmMap;
+
+	public Wchae() {
+		algorithmMap = new HashMap<>();
+	}
+
+	public String solveAlgorithm(Algorithm algorithm) {
+		Integer solveCount = algorithmMap.getOrDefault(algorithm, 0);
+		// 풀어보지 못한 알고리즘인 경우
+		if (algorithmMap.keySet()
+				.stream()
+				.filter(x -> x.isSolution(algorithm))
+				.findAny().isEmpty()) {
+			algorithmMap.put(algorithm, 1);
+			return "스트레스 받네...";
+		}
+		// 풀어본 알고리즘인 경우
+		boolean isSolved = Math.random() * 10 > solveCount;
+		if (isSolved) {
+			if (solveCount < 10) {
+				algorithmMap.put(algorithm, solveCount + 1);
+			}
+			return algorithm.getSolution();
+		}
+		return "스트레스 받네...";
+	}
 }
