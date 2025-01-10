87b6160c94416028f3db64bd1ed8c97daf63f7d7
duckgii <shinyonggi@naver.com>
2025-01-10 14:23:33 +0900

    FIX : checkPoint 출력 멘트 상수로 수정

--- v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Jpark2.java
+++ v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Jpark2.java
@@ -5,10 +5,14 @@ import java.util.List;
 import java.util.Random;
 
 public class Jpark2 {
-	private final ArrayList<String> ments; // final을 붙이면 arraylist 객체의 주소를 변경할 수 없는거지 ArrayList 클래스 내부 메서드를 이용한 값 변경은 가능하다
+	private final static String PREVOKE_MENT1 = "당신의 지각비, 회식비로 대체되었다";
+	private final static String PREVOKE_MENT2 = "코딩 그렇게 하는거 아닌데";
+	private final static String PREVOKE_MENT3 = "오늘 저녁은 감탄계";
+	private final static String POINTCHECK_MENT = "현재 대욱의 분노 수치 : %d\n";
+	private final ArrayList<String> ments;
 
 	Jpark2() {
-		ments = new ArrayList<>(List.of("당신의 지각비, 회식비로 대체되었다", "코딩 그렇게 하는거 아닌데", "오늘 저녁은 감탄계"));
+		ments = new ArrayList<>(List.of(PREVOKE_MENT1, PREVOKE_MENT2, PREVOKE_MENT3));
 	}
 
 	public String prevoke() {
@@ -19,6 +23,6 @@ public class Jpark2 {
 	}
 
 	public void checkPoint(int angryPoint) {
-		System.out.printf("현재 대욱의 분노 수치 : %d\n", angryPoint);
+		System.out.printf(POINTCHECK_MENT, angryPoint);
 	}
 }
