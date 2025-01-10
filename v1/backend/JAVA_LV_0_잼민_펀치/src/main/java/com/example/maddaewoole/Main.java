6764cd139d89d3fc7fdd645b7db6aaf2e6bff2cc
duckgii <shinyonggi@naver.com>
2025-01-10 14:25:53 +0900

    FIX : 메인 클래스 출력문 상수로 수정

--- v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Main.java
+++ v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Jpark2 jpark2 = new Jpark2();
 		Daewoole daewoole = new Daewoole();
+		final String PRINT_MENT = "지원은 '%s'를 시전하여 대욱의 분노를 %d 중가시켰다.\n";
 
 		while (daewoole.getPunchFlag()) {
 			String ment;
@@ -12,7 +13,7 @@ public class Main {
 
 			ment = jpark2.prevoke();
 			prevokePoint = daewoole.bePrevoked(ment);
-			System.out.printf("지원은 '%s'를 시전하여 대욱의 분노를 %d 중가시켰다.\n", ment, prevokePoint);
+			System.out.printf(PRINT_MENT, ment, prevokePoint);
 			jpark2.checkPoint(daewoole.getAngryPoint());
 			System.out.println();
 			if (!daewoole.getPunchFlag()) {
