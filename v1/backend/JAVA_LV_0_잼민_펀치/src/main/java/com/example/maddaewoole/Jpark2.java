de9a57d49858aa17818fbd7e128598b3673e5abc
duckgii <shinyonggi@naver.com>
2025-01-10 13:41:45 +0900

    FIX : Jpark2 프로퍼티 이름 ments로 변경, 생성 방식 변경(List.of 사용)

--- v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Jpark2.java
+++ v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Jpark2.java
@@ -1,23 +1,21 @@
 package com.example.maddaewoole;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Jpark2 {
-	private final ArrayList<String> ment; // final을 붙이면 arraylist 객체의 주소를 변경할 수 없는거지 ArrayList 클래스 내부 메서드를 이용한 값 변경은 가능하다
+	private final ArrayList<String> ments; // final을 붙이면 arraylist 객체의 주소를 변경할 수 없는거지 ArrayList 클래스 내부 메서드를 이용한 값 변경은 가능하다
 
 	Jpark2() {
-		ment = new ArrayList<String>();
-		ment.add("당신의 지각비, 회식비로 대체되었다");
-		ment.add("코딩 그렇게 하는거 아닌데");
-		ment.add("오늘 저녁은 감탄계");
+		ments = new ArrayList<>(List.of("당신의 지각비, 회식비로 대체되었다", "코딩 그렇게 하는거 아닌데", "오늘 저녁은 감탄계"));
 	}
 
 	public String prevoke() {
 		Random random = new Random();
-		int idx = random.nextInt(ment.size());
+		int idx = random.nextInt(ments.size());
 
-		return (ment.get(idx));
+		return (ments.get(idx));
 	}
 
 	public void checkPoint(int angryPoint) {
