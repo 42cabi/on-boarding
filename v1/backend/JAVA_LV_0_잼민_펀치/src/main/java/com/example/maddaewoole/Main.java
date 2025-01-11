e3a1d03b55f5f23bfe5361b2ac95fcc04a5beb3d
lamodadite <nmb11921@naver.com>
2025-01-11 15:51:30 +0900

    FIX : main의 출력부를 Util에 메서드로 분리

--- v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Main.java
+++ v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Main.java
@@ -1,22 +1,20 @@
 package com.example.maddaewoole;
 
+import static com.example.maddaewoole.Utils.printDaewoolePunchedJpark2;
+import static com.example.maddaewoole.Utils.printJpark2ProvokeDaewoole;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Jpark2 jpark2 = new Jpark2();
 		Daewoole daewoole = new Daewoole();
 		int cnt = 0;
-		while (true) {
+		while (!daewoole.isOverAngerLimit()) {
 			String ment = jpark2.provocate();
 			int anger = daewoole.provoked(ment);
-			System.out.printf("지원은 '%s'를 시전하여 대욱의 분노를 %d 증가시켰다.\n현재 대욱의 분노 수치 : %d\n\n", ment,
-					anger,
-					daewoole.getAngerLevel());
+			printJpark2ProvokeDaewoole(ment, anger, daewoole.getAngerLevel());
 			cnt++;
-			if (daewoole.isOverAngerLimit()) {
-				System.out.printf("참지 못한 대욱은 결국 지원에게 잼민 펀치를 날렸다.\n대욱을 도발한 횟수 : %d회\n", cnt);
-				break;
-			}
 		}
+		printDaewoolePunchedJpark2(cnt);
 	}
 }
--- v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Utils.java
+++ v1/backend/JAVA_LV_0_잼민_펀치/src/main/java/com/example/maddaewoole/Utils.java
@@ -9,4 +9,14 @@ public class Utils {
 		Random random = ThreadLocalRandom.current();
 		return random.nextInt(min, max);
 	}
+
+	public static void printJpark2ProvokeDaewoole(String ment, int anger, int daewooleAngerLevel) {
+		System.out.printf("지원은 '%s'를 시전하여 대욱의 분노를 %d 증가시켰다.\n현재 대욱의 분노 수치 : %d\n\n", ment,
+				anger,
+				daewooleAngerLevel);
+	}
+
+	public static void printDaewoolePunchedJpark2(int provokedCnt) {
+		System.out.printf("참지 못한 대욱은 결국 지원에게 잼민 펀치를 날렸다.\n대욱을 도발한 횟수 : %d회\n", provokedCnt);
+	}
 }
