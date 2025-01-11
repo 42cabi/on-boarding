9774b2bdd7090048833f658925246a0729b39e3f
lamodadite <nmb11921@naver.com>
2025-01-11 17:20:31 +0900

    FIX : 출력 내용 수정 및 출력부 메서드로 분리

--- v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/Main.java
+++ v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/Main.java
@@ -12,9 +12,18 @@ public class Main {
 		for (int i = 0; i < 20; i++) {
 			Algorithm first = algorithmList.get((int) (Math.random() * 5));
 			Algorithm second = algorithmList.get((int) (Math.random() * 5));
-			System.out.println(wchae.solveAlgorithm(first));
-			System.out.println(wchae.solveAlgorithm(second));
+			printTodaysAlgorithm(first, second);
+			printWchaeSolveResult(wchae.solveAlgorithm(first), wchae.solveAlgorithm(second));
 			System.out.println();
 		}
+	}
+
+	public static void printTodaysAlgorithm(Algorithm first, Algorithm second) {
+		System.out.printf("오늘의 알고리즘 : %s, %s\n", first.getClass().getSimpleName(),
+				second.getClass().getSimpleName());
+	}
+
+	public static void printWchaeSolveResult(String firstResult, String secondResult) {
+		System.out.printf("우주 : %s / %s\n", firstResult, secondResult);
 	}
 }
