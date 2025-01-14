fe009af0f617312bc11031b7ec75cc364dd4da87
soonvro <soonvro@gmail.com>
2025-01-14 17:16:45 +0900

    [BE] FEAT: 주문완료 기능 추가

--- v1/backend/JAVA_LV_1_알바의_달인/src/main/java/com/example/bot/BotImpl.java
+++ v1/backend/JAVA_LV_1_알바의_달인/src/main/java/com/example/bot/BotImpl.java
@@ -1,14 +1,29 @@
 package com.example.bot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BotImpl implements JiwonBehavior {
+	private Map<String, Integer> shoppingCart = new HashMap<>();
 
 	@Override
 	public void takeOrder(String order) {
-
 	}
 
 	@Override
 	public void result() {
+		System.out.printf("총 %d원 입니다\n", calculateTotalPrice());
+	}
 
+	private int getPrice(String product) {
+		return product.length();
+	}
+
+	private int calculateTotalPrice() {
+		int price = 0;
+		for (String product : shoppingCart.keySet()) {
+			price = getPrice(product) * shoppingCart.get(product);
+		}
+		return price;
 	}
 }
