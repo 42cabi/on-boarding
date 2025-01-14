8161417749631cd05fba7d6336ebf9f05b01a83e
lamodadite <nmb11921@naver.com>
2025-01-14 16:55:37 +0900

    FEAT : 서비스의 lent 함수 구현 && Cabinet 엔티티에 상태 바꾸는 메서드 추가

--- v1/backend/SPRING_LV_2_개노답_3계층/src/main/java/com/example/lent/LentServiceImpl.java
+++ v1/backend/SPRING_LV_2_개노답_3계층/src/main/java/com/example/lent/LentServiceImpl.java
@@ -1,12 +1,71 @@
 package com.example.lent;
 
+import static com.example.lent.domain.CabinetConfig.LENDING_PERIOD;
+
+import com.example.lent.domain.Cabinet;
+import com.example.lent.domain.CabinetStatus;
+import com.example.lent.domain.LentHistory;
+import com.example.lent.domain.User;
 import com.example.lent.dto.LentRequest;
 import com.example.lent.dto.LentResponse;
+import com.example.lent.testutil.CabinetRepository;
+import com.example.lent.testutil.UserRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 /*서비스는 뭘까요?*/
+@RequiredArgsConstructor
 public class LentServiceImpl implements LentService {
+
+	private final LentHistoryRepository lentHistoryRepository;
+	private final CabinetRepository cabinetRepository;
+	private final UserRepository userRepository;
+
 	@Override
 	public LentResponse lent(LentRequest request) {
-		return null;
+		Cabinet findCabinet = cabinetRepository.findAll()
+				.stream()
+				.filter(cabinet -> cabinet.getCabinetId() == request.getCabinetId())
+				.findFirst()
+				.orElseThrow(() -> new RuntimeException(
+						request.getCabinetId() + "번 사물함이 존재하지 않습니다"));//TODO: 커스텀 예외 클래스 만들기
+
+		User findUser = userRepository.findById(request.getUserId());
+		List<LentHistory> lentHistoryList = lentHistoryRepository.findAll();
+
+		// 사물함의 상태가 사용중인 경우
+		if (findCabinet.getCabinetStatus() == CabinetStatus.FULL) {
+			throw new RuntimeException("이미 사용 중인 사물함입니다.");
+		}
+
+		// 유저가 현재 사물함을 대여중인 경우
+		boolean isUserLent = lentHistoryList.stream()
+				.findFirst()
+				.filter(lentHistory -> lentHistory.getLentUserName() == findUser.getName())
+				.isPresent(); // TODO: 가독성이 안좋으니 메서드로 빼기?
+		if (isUserLent) {
+			throw new RuntimeException("이미 대여 중인 유저입니다.");
+		}
+
+		// 사용 정지 상태인 유저인 경우
+		if (findUser.isBanned()) {
+			throw new RuntimeException("사용 정지상태인 유저입니다.");
+		}
+
+		findCabinet.changeStatus(CabinetStatus.FULL);
+		cabinetRepository.save(findCabinet);
+
+		LentHistory lentHistory = lentHistoryRepository.save(new LentHistory(
+				findCabinet.getCabinetId(),
+				findUser.getUserId(),
+				findUser.getName(),
+				request.getCreatedAt(),
+				request.getCreatedAt().plusDays(LENDING_PERIOD)));
+
+		return new LentResponse(lentHistory.getLentHistoryId(),
+				lentHistory.getCabinetId(),
+				findUser.getName(),
+				lentHistory.getCreatedAt(),
+				lentHistory.getExpiredAt());
 	}
 }
