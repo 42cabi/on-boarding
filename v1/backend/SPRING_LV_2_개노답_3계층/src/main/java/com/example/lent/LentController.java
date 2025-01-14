dba1e6973e4aa4c6189de7643614aa627e1f69ea
lamodadite <nmb11921@naver.com>
2025-01-14 16:51:24 +0900

    FEAT : 컨트롤러 lent 기능 구현

--- v1/backend/SPRING_LV_2_개노답_3계층/src/main/java/com/example/lent/LentController.java
+++ v1/backend/SPRING_LV_2_개노답_3계층/src/main/java/com/example/lent/LentController.java
@@ -2,13 +2,16 @@ package com.example.lent;
 
 import com.example.lent.dto.LentRequest;
 import com.example.lent.dto.LentResponse;
+import lombok.RequiredArgsConstructor;
 
 /*컨트롤러는 뭘까요?*/
 /*컨트롤러는 왜 인터페이스가 없어도 될까요?*/
+@RequiredArgsConstructor
 public class LentController {
-	private LentService lentService;
+
+	private final LentService lentService;
 
 	public LentResponse lent(LentRequest request) {
-		return null;
+		return lentService.lent(request);
 	}
 }
---
+++ v1/backend/SPRING_LV_2_개노답_3계층/src/main/java/com/example/lent/domain/CabinetConfig.java
@@ -0,0 +1,4 @@
+package com.example.lent.domain;
+
+public class CabinetConfig {
+}
