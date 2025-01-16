a8899248d73a921bd58ee478693728078bfbab6d
Minkyu01 <koryum30@gmail.com>
2025-01-16 17:53:16 +0900

    [FE] FEAT: v2 프론트엔드 기초 설정

---
+++ v2/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "./MainPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
