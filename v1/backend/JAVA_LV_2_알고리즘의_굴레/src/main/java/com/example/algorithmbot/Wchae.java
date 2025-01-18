a4aed70a23925a865ba60edf5b2677e6d8334c5d
soonvro <soonvro@gmail.com>
2025-01-18 23:52:42 +0900

    [BE] FEAT: 알고리즘 푼 횟수 get 기능 추가

--- v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/Wchae.java
+++ v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/Wchae.java
@@ -1,5 +1,18 @@
 package com.example.algorithmbot;
 
-public class Wchae {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Wchae {
+	private Map<Algorithm, Integer> algorithmMap = new HashMap<>();
+
+	public void solveAlgorithm(Algorithm algorithm) {}
+
+	private int getSolveCount(Algorithm algorithm) {
+		return algorithmMap.entrySet().stream()
+				.filter(entry -> entry.getKey().isSolution(algorithm))
+				.map(Map.Entry::getValue)
+				.findAny()
+				.orElse(0);
+	}
 }
