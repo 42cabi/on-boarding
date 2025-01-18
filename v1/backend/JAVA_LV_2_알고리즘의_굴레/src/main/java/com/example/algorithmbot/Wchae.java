e57f32c957b24124082ed2761822812503b69e86
soonvro <soonvro@gmail.com>
2025-01-19 00:17:52 +0900

    [BE] FEAT: 알고리즘 해결 확률 계산 기능 추가

--- v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/Wchae.java
+++ v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/Wchae.java
@@ -4,9 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Wchae {
+	// Constants --------------------------------------------------------------
+	private static final double INITIAL_PROBABILITY = 0.1;
+	private static final double PROBABILITY_INCREMENT = 0.1;
+	private static final double MAX_PROBABILITY = 1.0;
+	// ------------------------------------------------------------------------
+
 	private Map<Algorithm, Integer> algorithmMap = new HashMap<>();
 
-	public void solveAlgorithm(Algorithm algorithm) {}
+	public void solveAlgorithm(Algorithm algorithm) {
+	}
 
 	private int getSolveCount(Algorithm algorithm) {
 		return algorithmMap.entrySet().stream()
@@ -14,5 +21,10 @@ public class Wchae {
 				.map(Map.Entry::getValue)
 				.findAny()
 				.orElse(0);
+	}
+
+	private double calculateSolveProbability(int solveCount) {
+		return Math.min(INITIAL_PROBABILITY + PROBABILITY_INCREMENT * solveCount,
+				MAX_PROBABILITY);
 	}
 }
