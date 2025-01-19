7cee49f9529b4f21b7b1f8c0a5e626918ebf9d16
soonvro <soonvro@gmail.com>
2025-01-19 14:46:45 +0900

    [BE] FEAT: TwoPointer isSolution getSolution 구현

--- v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/TwoPointer.java
+++ v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/TwoPointer.java
@@ -1,5 +1,13 @@
 package com.example.algorithmbot;
 
 public class TwoPointer implements Algorithm {
+	private static final String SOLUTION = "너에게 닿기를…";
 
+	public boolean isSolution(Algorithm algorithm) {
+		return (algorithm instanceof TwoPointer);
+	}
+
+	public String getSolution() {
+		return SOLUTION;
+	}
 }
