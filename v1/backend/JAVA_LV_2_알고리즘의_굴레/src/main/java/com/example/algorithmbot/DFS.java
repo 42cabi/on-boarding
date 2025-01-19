64d3fd3715cb3daf3ef6be3f54a4e504a6932c06
soonvro <soonvro@gmail.com>
2025-01-19 14:45:36 +0900

    [BE] FEAT: DFS isSolution getSolution 구현

--- v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/DFS.java
+++ v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/DFS.java
@@ -1,5 +1,13 @@
 package com.example.algorithmbot;
 
 public class DFS implements Algorithm {
+	private static final String SOLUTION = "나는 한 놈씩 조져";
 
+	public boolean isSolution(Algorithm algorithm) {
+		return (algorithm instanceof DFS);
+	}
+
+	public String getSolution() {
+		return SOLUTION;
+	}
 }
