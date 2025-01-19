c467ce223517f501bb337274f0fa0a3d60a6f9c9
soonvro <soonvro@gmail.com>
2025-01-19 14:46:13 +0900

    [BE] FEAT: BinarySearch isSolution getSolution 구현

--- v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/BinarySearch.java
+++ v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/BinarySearch.java
@@ -1,5 +1,13 @@
 package com.example.algorithmbot;
 
 public class BinarySearch implements Algorithm {
+	private static final String SOLUTION = "반띵 ㄱㄱ";
 
+	public boolean isSolution(Algorithm algorithm) {
+		return (algorithm instanceof BinarySearch);
+	}
+
+	public String getSolution() {
+		return SOLUTION;
+	}
 }
