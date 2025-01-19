b05750d2785e4f69844fc8144d1ec94cb87d4a18
soonvro <soonvro@gmail.com>
2025-01-19 14:45:48 +0900

    [BE] FEAT: DP isSolution getSolution 구현

--- v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/DP.java
+++ v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/DP.java
@@ -1,5 +1,13 @@
 package com.example.algorithmbot;
 
 public class DP implements Algorithm {
+	private static final String SOLUTION = "누가 첫날부터 DP문제를 들고옵니까";
 
+	public boolean isSolution(Algorithm algorithm) {
+		return (algorithm instanceof DP);
+	}
+
+	public String getSolution() {
+		return SOLUTION;
+	}
 }
