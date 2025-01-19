274ef803324c04145bfed21cf11edf4afcada3d3
soonvro <soonvro@gmail.com>
2025-01-19 14:45:25 +0900

    [BE] FEAT: BFS isSolution getSolution 구현

--- v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/BFS.java
+++ v1/backend/JAVA_LV_2_알고리즘의_굴레/src/main/java/com/example/algorithmbot/BFS.java
@@ -1,5 +1,13 @@
 package com.example.algorithmbot;
 
 public class BFS implements Algorithm {
+	private static final String SOLUTION = "주변부터 둘러보기";
 
+	public boolean isSolution(Algorithm algorithm) {
+		return (algorithm instanceof BFS);
+	}
+
+	public String getSolution() {
+		return SOLUTION;
+	}
 }
