8ffec730d9a5b8fa3fb8900434d87d7eeeded051
duckgii <shinyonggi@naver.com>
2025-01-20 17:42:13 +0900

    ETC : add comment

--- v1/backend/SPRING_LV_1_콩_심은_데_콩_난다/src/main/java/com/example/kong/KongController.java
+++ v1/backend/SPRING_LV_1_콩_심은_데_콩_난다/src/main/java/com/example/kong/KongController.java
@@ -1,11 +1,18 @@
 package com.example.kong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /* Spring Boot에서 컨트롤러는 어떻게 인식할까요? */
+@RestController // controller + ResponseBody -> json형태로 객체 반환
 public class KongController {
 
-	private KongService kongService;
+	private final KongService kongService;
+
+
+	public KongController(KongService kongService) {
+		this.kongService = kongService;
+	}
 
 	@GetMapping("/reaction")
 	public String reaction() {
