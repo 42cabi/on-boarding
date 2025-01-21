a54ccb834ab9bb342c5058d4a3b3de9080dd208a
JunSeong <gentlmanjun@naver.com>
2025-01-21 20:33:08 +0900

    [FE] FEAT: 커스텀 axios 인스턴스와 API 서비스 구현

---
+++ v2/frontend/src/api/users.ts
@@ -0,0 +1,21 @@
+import { service } from "./axios.custom";
+
+export const checkAuth = async () => {
+  return service.get("/users/auth");
+};
+
+export const login = async (data: object) => {
+  return service.post("/users/login", data);
+};
+
+export const register = async (data: object) => {
+  return service.post("/users/register", data);
+};
+
+export const searchGroup = async (params: object) => {
+  return service.get(`/users/search/groups`, { params });
+};
+
+export const searchName = async (params: object) => {
+  return service.get(`/users/search/names`, { params });
+};
