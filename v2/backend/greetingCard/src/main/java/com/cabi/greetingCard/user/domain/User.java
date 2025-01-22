299c21d04765f326e16d22ff3a8eb42d30310633
lamodadite <nmb11921@naver.com>
2025-01-22 15:35:52 +0900

    TEST : H2 test를 위해 테이블 이름 User -> Users로 변경 (User가 h2의 예약어라서 테이블 생성이 안됨)

--- v2/backend/greetingCard/src/main/java/com/cabi/greetingCard/user/domain/User.java
+++ v2/backend/greetingCard/src/main/java/com/cabi/greetingCard/user/domain/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Table(name = "USERS")
 public class User {
 
 	@Id
