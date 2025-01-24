81647d032d36d5e0166d1ce0bbeb1365f9584891
Jihyun Kim <ksk208105@gmail.com>
2025-01-24 12:21:40 +0900

    [FE] FIX: BE 포트번호 변경

--- v2/frontend/src/api/axios.custom.ts
+++ v2/frontend/src/api/axios.custom.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api", // 기본 URL
+  baseURL: "http://localhost:8080/api", // 기본 URL
   headers: {
     "Content-Type": "application/json",
   },
