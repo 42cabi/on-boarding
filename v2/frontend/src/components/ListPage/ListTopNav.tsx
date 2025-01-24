b45c0cc44feffc912d11016fb815fe024ece1055
JunSeong <gentlmanjun@naver.com>
2025-01-24 16:26:16 +0900

    [FE] FEAT: 네비게이션 구현

---
+++ v2/frontend/src/components/ListPage/ListTopNav.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router";
+
+const ListTopNav = () => {
+  return (
+    <nav>
+      <Link to="/send">덕담 보내러 가기</Link>
+    </nav>
+  );
+};
+
+export default ListTopNav;
