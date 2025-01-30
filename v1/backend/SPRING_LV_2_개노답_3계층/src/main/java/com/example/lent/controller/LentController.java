c7cc7cd55ed1b947b57d4392f6cabf8e74b26863
soonvro <soonvro@gmail.com>
2025-01-30 23:35:50 +0900

    [BE] REFACTOR: 패키지 구조 변경

--- v1/backend/SPRING_LV_2_개노답_3계층/src/main/java/com/example/lent/controller/LentController.java
+++ v1/backend/SPRING_LV_2_개노답_3계층/src/main/java/com/example/lent/controller/LentController.java
@@ -1,7 +1,8 @@
-package com.example.lent;
+package com.example.lent.controller;
 
 import com.example.lent.dto.LentRequest;
 import com.example.lent.dto.LentResponse;
+import com.example.lent.service.LentService;
 
 /*컨트롤러는 뭘까요?*/
 /*컨트롤러는 왜 인터페이스가 없어도 될까요?*/
