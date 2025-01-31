1efcd2474529ccefde02afc2d9ebdced32c443ec
soonvro <soonvro@gmail.com>
2025-01-31 14:06:49 +0900

    [BE] FEAT: UserRepository, CabinetRepository 의존성 주입

--- v1/backend/SPRING_LV_2_개노답_3계층/src/main/java/com/example/lent/service/LentServiceImpl.java
+++ v1/backend/SPRING_LV_2_개노답_3계층/src/main/java/com/example/lent/service/LentServiceImpl.java
@@ -3,12 +3,16 @@ package com.example.lent.service;
 import com.example.lent.dto.LentRequest;
 import com.example.lent.dto.LentResponse;
 import com.example.lent.repository.LentHistoryRepository;
+import com.example.lent.testutil.CabinetRepository;
+import com.example.lent.testutil.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 /*서비스는 뭘까요?*/
 @RequiredArgsConstructor
 public class LentServiceImpl implements LentService {
 	private final LentHistoryRepository lentHistoryRepository;
+	private final CabinetRepository cabinetRepository;
+	private final UserRepository userRepository;
 
 	@Override
 	public LentResponse lent(LentRequest request) {
