006794b2fd4fc861440b254723604b5bb76c61a6
soonvro <soonvro@gmail.com>
2025-01-31 14:03:02 +0900

    [BE] FEAT: LentHistoryRepository 의존성 주입

--- v1/backend/SPRING_LV_2_개노답_3계층/src/main/java/com/example/lent/service/LentServiceImpl.java
+++ v1/backend/SPRING_LV_2_개노답_3계층/src/main/java/com/example/lent/service/LentServiceImpl.java
@@ -2,9 +2,14 @@ package com.example.lent.service;
 
 import com.example.lent.dto.LentRequest;
 import com.example.lent.dto.LentResponse;
+import com.example.lent.repository.LentHistoryRepository;
+import lombok.RequiredArgsConstructor;
 
 /*서비스는 뭘까요?*/
+@RequiredArgsConstructor
 public class LentServiceImpl implements LentService {
+	private final LentHistoryRepository lentHistoryRepository;
+
 	@Override
 	public LentResponse lent(LentRequest request) {
 		return null;
