e2133d96dd4a761a7d03fd9778c5b931e20600d6
soonvro <soonvro@gmail.com>
2025-01-31 13:37:13 +0900

    [BE] FEAT: LentService 의존성 주입 추가

--- v1/backend/SPRING_LV_2_개노답_3계층/src/main/java/com/example/lent/controller/LentController.java
+++ v1/backend/SPRING_LV_2_개노답_3계층/src/main/java/com/example/lent/controller/LentController.java
@@ -3,11 +3,13 @@ package com.example.lent.controller;
 import com.example.lent.dto.LentRequest;
 import com.example.lent.dto.LentResponse;
 import com.example.lent.service.LentService;
+import lombok.RequiredArgsConstructor;
 
 /*컨트롤러는 뭘까요?*/
 /*컨트롤러는 왜 인터페이스가 없어도 될까요?*/
+@RequiredArgsConstructor
 public class LentController {
-	private LentService lentService;
+	private final LentService lentService;
 
 	public LentResponse lent(LentRequest request) {
 		return null;
