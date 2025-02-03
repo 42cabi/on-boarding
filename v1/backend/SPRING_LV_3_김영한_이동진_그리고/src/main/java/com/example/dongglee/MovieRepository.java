a090636757f21ef60f1b0250ce98a6f8b48956c7
soonvro <soonvro@gmail.com>
2025-02-03 21:32:19 +0900

    [BE] REFACTOR: 불필요한 import문 제거

--- v1/backend/SPRING_LV_3_김영한_이동진_그리고/src/main/java/com/example/dongglee/MovieRepository.java
+++ v1/backend/SPRING_LV_3_김영한_이동진_그리고/src/main/java/com/example/dongglee/MovieRepository.java
@@ -4,8 +4,6 @@ import com.example.dongglee.domain.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
