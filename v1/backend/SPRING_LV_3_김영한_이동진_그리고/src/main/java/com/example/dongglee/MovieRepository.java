c66b4327588b27891b891a1bc22f5626accad5f9
soonvro <soonvro@gmail.com>
2025-02-03 21:29:39 +0900

    [BE] FIX: MovieRepository가 JpaRepository를 상속하도록 변경

--- v1/backend/SPRING_LV_3_김영한_이동진_그리고/src/main/java/com/example/dongglee/MovieRepository.java
+++ v1/backend/SPRING_LV_3_김영한_이동진_그리고/src/main/java/com/example/dongglee/MovieRepository.java
@@ -1,13 +1,11 @@
 package com.example.dongglee;
 
 import com.example.dongglee.domain.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MovieRepository {
-	List<Movie> findAll();
-
-	Movie save(Movie movie);
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
